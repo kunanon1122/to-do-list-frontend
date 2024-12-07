c5080d813006fc3f4c36a819e53a62d83f24caad
Kunanon <kunanon@drivehub.co>
2024-12-07 14:57:21 +0700

    setup tailwind css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,7 @@
-import localFont from "next/font/local";
+import React from "react";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const Home = () => {
+  return <div className="text-3xl text-core-red-100">Hello world</div>;
+};
 
-export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        hello world
-      </main>
-    </div>
-  );
-}
+export default Home;
