a0492fa49462fea0575d1f94175a2dce61193e6e
Kunanon <kunanon@drivehub.co>
2024-12-18 20:30:10 +0700

    remove change language from local storage

--- src/features/LanguageButton/index.tsx
+++ src/features/LanguageButton/index.tsx
@@ -7,23 +7,13 @@ import Button from "@/components/Button";
 const LanguageButton: FC = () => {
   const { i18n } = useTranslation();
   const router = useRouter();
-  const { locale } = router;
 
   const handleChangeLanguage = () => {
     const changeLanguage = i18n.language === "th" ? "en" : "th";
 
     i18n.changeLanguage(changeLanguage);
-    localStorage.setItem("locale", changeLanguage);
     router.push(router.pathname, router.asPath, { locale: changeLanguage });
   };
-
-  useEffect(() => {
-    const savedLocale = localStorage.getItem("locale");
-
-    if (savedLocale && savedLocale !== locale) {
-      router.push(router.pathname, router.asPath, { locale: savedLocale });
-    }
-  }, [locale, router]);
 
   return (
     <Button theme="blue" onClick={handleChangeLanguage}>
